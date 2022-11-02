f8f0138b1ed52eb26b9785f0e130523374c4fab3
kokomanu31 <emanuel.kokomanu31@gmail.com>
2022-11-02 18:14:38 -0400

    Add database and most of CRUD
    - Added MongoDB to project
    - Added possibility to save rating in database
    - Added possibility to view own rating for a movie
    - Added possibility to remove own rating for a movie
    - Added disconnect command to safely close database connection and exit script (to be tested)
    - Changed README.md to reflect progress made

--- README.md
+++ README.md
@@ -6,23 +6,24 @@ MovRater is a Discord bot that allows server members to rate and get ratings for
 
 ### Project objective
 Have a discord bot able to:
-- Save user ratings
-- Display ratings
-- Change/remove ratings
+- Save user's movie rating 
+- Display a user's rating
+- Change user's movie rating
+- Remove user's movie rating
 - Show top rated movies from the server
 - Show lesser rated movies from the server
 - Show top rated movies per category
 
-*Maybe we'll have a website that will be able to show all the informations as well as through bot commands.*
+*Maybe have a website down the road that will be able to show all the same informations (view only) as the bot commands.*
 
 ### 🚧 Future functionalities & bug fixes 🚧
-- Make command take IMDB link as a parameter
-- Make command print user ID instead of username
-- Use local database to save data
-- Display rating from a user for a specific movie
+- View another user's rating for a movie
 - Change rating for a specific movie
-- Remove rating for a specific movie
-- Change command to a webhook with reactions for ratings instead of command options
+- Display count of ratings for a movie
+- Add created/modified date to ratings
+- Change command to a webhook with reactions/buttons for ratings instead of command options
+- Make response hidden to all but user
+- Make sure script is exited properly
 
 ### Watch me programming LIVE
 I work on most of my projects live on [Twitch](https://www.twitch.tv/kokomanu_31) so if you ever want to see everything I do feel free to drop by!
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
-import string
 import discord
+import pymongo
+import certifi
 from discord import app_commands
 from dotenv import load_dotenv
 
@@ -8,24 +9,30 @@ from dotenv import load_dotenv
 # https://discord.com/developers/applications
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CONNECTION_STRING = os.getenv('DB_CONNECTION_STR')
 DEV_GUILD = discord.Object(id=956313821914484806)
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True)
-client = discord.Client(intents = intents)
-tree = app_commands.CommandTree(client)
+bot_client = discord.Client(intents = intents)
+tree = app_commands.CommandTree(bot_client)
 
-@client.event
+# Connect to database
+ca = certifi.where()
+db_client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile = ca)
+db_collection = db_client["MovRater"]["ratings"]
+
+@bot_client.event
 async def on_ready():
     # Refresh command list (remove guild when in production, may take to 1h to execute command)
     await tree.sync(guild = DEV_GUILD)
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot_client.user} has connected to Discord!')
 
 # Ping command
 @tree.command(name = "ping", description = "Check if the bot is online and able to receive commands", guild = DEV_GUILD)
-async def test_command(interaction):
+async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
 
-# Add Rating command
+# Add rating command
 @tree.command(name = "add_rating", description = "Add a movie or TV show rating to the database", guild = DEV_GUILD)
 @app_commands.choices(rating = [
     app_commands.Choice(name = "Amazing", value = "⭐"),
@@ -35,7 +42,57 @@ async def test_command(interaction):
     app_commands.Choice(name = "Terrible", value = "🗑️")
 ])
 async def add_rating(interaction: discord.Interaction, movie_link: str, rating: app_commands.Choice[str]):
-    await interaction.response.send_message(str(interaction.user.id) + " added a " + rating.value + " rating for the movie " + movie_link)
+    discord_user = interaction.user
 
-# Make bot come online
-client.run(TOKEN)
+    # Check if user already rated movie
+    if (db_collection.count_documents({"discord_id": discord_user.id, "movie_link": movie_link}) > 0):
+        await interaction.response.send_message(f"It seems you've already rated this movie <@{str(discord_user.id)}>. Please remove your rating with the `/remove_rating` before rating it again.")
+    else:
+        x = db_collection.insert_one({"discord_id": discord_user.id, "movie_link": movie_link, "rating": rating.name})
+        print(f"Inserted movie for {discord_user.id} ({discord_user.name}) with document id={str(x.inserted_id)}")
+        await interaction.response.send_message(f"<@{str(discord_user.id)}> added a {rating.value} rating for the movie {movie_link}")
+
+# View rating command
+@tree.command(name = "view_rating", description = "View your rating of a movie or TV show from the database", guild = DEV_GUILD)
+async def view_rating(interaction: discord.Interaction, movie_link: str):
+    discord_user = interaction.user
+
+    x = db_collection.find_one({"discord_id": discord_user.id, "movie_link": movie_link})
+    if (x != None):
+        if (x["rating"] == "Amazing"):
+            rating = "⭐"
+        elif (x["rating"] == "Good"):
+            rating = "👍"
+        elif (x["rating"] == "Meh"):
+            rating = "🤷‍♂️"
+        elif (x["rating"] == "Bad"):
+            rating = "👎"
+        elif (x["rating"] == "Terrible"):
+            rating = "🗑️"
+        
+        await interaction.response.send_message(f"<@{str(discord_user.id)}>, your rating for the movie {movie_link} is {rating}")
+    else:
+        await interaction.response.send_message(f"<@{str(discord_user.id)}>, you have yet to review that movie. Use the `/add_rating` command to add your rating for it.")
+
+# Remove rating command
+@tree.command(name = "remove_rating", description = "Remove your rating for a movie or TV show from the database", guild = DEV_GUILD)
+async def remove_rating(interaction: discord.Interaction, movie_link: str):
+    discord_user = interaction.user
+
+    x = db_collection.find_one_and_delete({"discord_id": discord_user.id, "movie_link": movie_link})
+    if (x != None):
+        await interaction.response.send_message(f"<@{str(discord_user.id)}>, your rating for that movie is removed")
+    else:
+        await interaction.response.send_message(f"<@{str(discord_user.id)}>, you have yet to review that movie. Use the `/add_rating` command to add your rating for it.")
+
+# Close connection to database
+@tree.command(name = "disconnect", description = "Close connection to the database and disconnect bot", guild = DEV_GUILD)
+async def disconnect(interaction: discord.Interaction):
+    db_client.close()
+    print("Connection to database successful! Disconecting...")
+    # TODO: Make sure bellow is proper way to exit discord and python script
+    await bot_client.close()
+    quit()
+
+# Connect bot to Discord
+bot_client.run(TOKEN)
